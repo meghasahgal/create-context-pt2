a316afe17f207a686462009bf07720ce283dadf9
Megha Sahgal <meghas@gmail.com>
2022-10-05 05:11:49 +0000

    1st & 2nd context practices

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -2,17 +2,18 @@ import { useContext } from "react";
 import { HoroscopeContext } from "../context/HoroscopeContext";
 
 const Detail = () => {
-  const horoscopesObj = useContext(HoroscopeContext);
-
+  const horoscopes = useContext(HoroscopeContext);
+  const { sign } = useContext(HoroscopeContext);
+  console.log(sign);
   return (
     <div className="details">
       <img
         src="https://upload.wikimedia.org/wikipedia/commons/e/e1/FullMoon2010.jpg"
         alt=""
       />
-      <h2>Current Sign Name</h2>
-      <h4>Element: </h4>
-      <h4>Traits: </h4>
+      <h2>{sign.name}</h2>
+      <h4>Element: {sign.element} </h4>
+      <h4>Traits: {sign.traits} </h4>
     </div>
   );
 };
